dfcebe9c3777b397ed9c10434d1eda70f789a053
Athar Mahboob <athar.cse1@gmail.com>
2020-12-22 23:09:28 +0530

    Splash Screen

---
+++ lib/screens/splash/components/body.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+import 'package:shop_app/constants.dart';
+import 'package:shop_app/screens/splash/components/splash_container.dart';
+import 'package:shop_app/size_config.dart';
+
+class Body extends StatefulWidget {
+  @override
+  _BodyState createState() => _BodyState();
+}
+
+class _BodyState extends State<Body> {
+  List<Map<String, String>> splashData = [
+    {
+      "text": "Welcome to Tokoto, Let's shop!",
+      "image": "assets/images/splash_1.png"
+    },
+    {
+      "text": "We help people connect with store \naround India",
+      "image": "assets/images/splash_2.png"
+    },
+    {
+      "text": "We show the easy way to shop. \nJust stay at home with us",
+      "image": "assets/images/splash_3.png"
+    },
+  ];
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: SizedBox(
+        width: double.infinity,
+        child: Column(
+          children: [
+            Expanded(
+              flex: 3,
+              child: PageView.builder(
+                itemCount: splashData.length,
+                itemBuilder: (context, index) => SplashContainer(
+                  text: splashData[index]["text"],
+                  image: splashData[index]["image"],
+                ),
+              ),
+            ),
+            Expanded(
+              flex: 2,
+              child: SizedBox(),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
